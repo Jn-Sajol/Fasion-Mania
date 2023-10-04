bfc1f68d368fcf85c70104cc2eff10f9da480b15
Jn-Sajol <julkar10121@gmail.com>
2023-10-04 16:42:03 +0600

    adding componenet

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+
+import CategoryItem from '../categori-item/category-item-components';
+import './directory.styles.scss';
+
+const Directory = ({ categories }) => {
+  return (
+    <div className='directory-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;

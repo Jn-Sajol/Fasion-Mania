d1d8067de43139cab4b440c2ff6e3f497d190154
Jn-Sajol <julkar10121@gmail.com>
2023-10-10 04:16:47 +0600

    start again cart context

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 import Button from '../button/button.component';
 
 import './cart-dropdown.styles.scss';
+import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => (
   <div className='cart-dropdown-container'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+    <CartItem/>
+    </div>
     <Button>GO TO CHECKOUT</Button>
   </div>
 );

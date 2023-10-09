106bd991d17ad2465e4f53b2df984ae0e025df96
Jn-Sajol <julkar10121@gmail.com>
2023-10-09 19:31:16 +0600

    adding cart context

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -10,8 +10,3 @@ export const CartProvider = ({ children }) => {
   const value = { isCartOpen, setIsCartOpen };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
-export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
-  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
-};

ab785c8a4273daffe2fc024fe2713630ec70d55b
Jn-Sajol <julkar10121@gmail.com>
2023-10-09 18:36:04 +0600

    adding cart item segmnt

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,8 +1,11 @@
 // import shopicon from '../../assets/shopping-bag.svg';
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import shopicon from "../../assets/shopping-bag.svg";
+import './cart-icon.styles.scss';
+
 const CartIcon = () => {
   return (
     <div className="cart-icon-container">
+      <ShoppingIcon className="shopping-icon" />
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>

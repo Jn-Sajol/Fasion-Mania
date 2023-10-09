1f30f24ba69cedbf15d2ed3c67cf4c11fed0c75a
Jn-Sajol <julkar10121@gmail.com>
2023-10-10 03:27:04 +0600

    start again cart context

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -14,9 +14,6 @@ const CartIcon = () => {
   return (
     <div className='cart-icon-container' onClick={toggleIsCartOpen}>
       <h5 className='shopping-icon'>icon</h5>
-      <h5 className='shopping-icon'>icon</h5>
-      <h5 className='shopping-icon'>icon</h5>
-      <h5 className='shopping-icon'>icon</h5>
       <span className='item-count'>0</span>
     </div>
   );

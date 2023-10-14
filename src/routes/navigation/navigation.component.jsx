a4b143929f9bf421e89b303a29948dc42edc2f6f
Jn-Sajol <julkar10121@gmail.com>
2023-10-14 23:14:14 +0600

    editing nav

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -75,6 +75,9 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
           about
           </Link>
+          <Link className='nav-link' to='/shop'>
+          about
+          </Link>
 
           {currentUser ? (
             <span className='nav-link' onClick={signOutUser}>
